d7279f296f65c81d360c76e5210d3cbf99989a62
hamzahubb <bcy212009@myu.edu.pk>
2026-08-25 20:30:04 +0500

    ci: add GitHub Actions workflow for Next.js GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  poweredByHeader: false,
-  images: {
-    unoptimized: true
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let repo = '';
+
+if (isGithubActions) {
+  const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] || '';
+  if (repoName && !repoName.endsWith('.github.io')) {
+    repo = `/${repoName}`;
   }
+}
+
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || repo,
 };
 
 export default nextConfig;
